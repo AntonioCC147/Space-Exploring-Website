653a7c8547cd7f7018cf2d82e7ab1c86b479772e
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-03-23 16:02:05 +0200

    Almost finish home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Explore from '../../assets/images/Explore.png';
+
 import './Home.css';
 
 export default function Home() {
@@ -15,6 +17,9 @@ export default function Home() {
                         Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on
                         the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
                     </p>
+                </Col>
+                <Col sm={12} md={6} className="exploreContainer">
+                    <img src={Explore} className="explore" alt="Explore"/>
                 </Col>
             </Row>
         </Container>
