96670524f7010545ee9c25713180a77a30fc180f
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-03-23 15:47:09 +0200

    Added text

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -9,6 +9,12 @@ import NavbarLine from '../../assets/icons/NavbarLine.png';
 import './Navbar.css';
 
 export default function NavbarComponent() {
+    const currentPath = window.location.pathname;
+
+    const isCurrentPage = (path) => {
+        return currentPath === path ? "navbarText1" : "navbarText2";
+    }
+
     return (
         <Navbar key="lg" expand="lg" fixed="top" className="navbarComponent mb-3 bellefair">
             <a href="/"><img src={NavbarStar} alt="NavabrStar" className="navabrStar"/></a>
@@ -23,10 +29,10 @@ export default function NavbarComponent() {
                     <Offcanvas.Header closeButton/>
                     <Offcanvas.Body>
                         <Nav className="justify-content-start flex-grow-1 pe-3">
-                            <Nav.Link href="/" className="navbarText"><b>00 </b>HOME</Nav.Link>
-                            <Nav.Link href="/" className="navbarText"><b>01 </b>PLANETS</Nav.Link>
-                            <Nav.Link href="/" className="navbarText"><b>02 </b>CREW</Nav.Link>
-                            <Nav.Link href="/" className="navbarText"><b>03 </b>TECHNOLOGY</Nav.Link>
+                            <Nav.Link href="/" className={isCurrentPage('/')}><b>00 </b>HOME</Nav.Link>
+                            <Nav.Link href="/1" className={isCurrentPage('/1')}><b>01 </b>PLANETS</Nav.Link>
+                            <Nav.Link href="/2" className={isCurrentPage('/2')}><b>02 </b>CREW</Nav.Link>
+                            <Nav.Link href="/3" className={isCurrentPage('/3')}><b>03 </b>TECHNOLOGY</Nav.Link>
                         </Nav>
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './Home.css';
 
 export default function Home() {
     return (
         <Container fluid className="principalBackground bellefair">
-            
+            <Row>
+                <Col sm={12} md={6}>
+                    <p className="titleHome1">SO, YOU WANT TO TRAVE TO</p>
+                    <p className="titleHome2">SPACE</p>
+                    <p className="titleHome3">
+                        Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on
+                        the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
+                    </p>
+                </Col>
+            </Row>
         </Container>
     )
 }
